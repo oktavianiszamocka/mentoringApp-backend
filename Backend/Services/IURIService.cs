1e5369bf4dde1ecfd149ce8a606df66715aff3f4
Tami <tami.kyanh@gmail.com>
2020-09-27 13:40:28 +0200

    Generating Pagination URLs

---
+++ Backend/Services/IURIService.cs
@@ -0,0 +1,13 @@
+﻿using MentorApp.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorApp.Services
+{
+    public interface IUriService
+    {
+        public Uri GetPageUri(PaginationFilter filter, string route);
+    }
+}
