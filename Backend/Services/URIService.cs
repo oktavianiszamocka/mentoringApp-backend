e8bf697456f6663d3c883bb8e829d45d9540faae
Tami <tami.kyanh@gmail.com>
2020-09-27 16:12:57 +0200

    Configuring the Service to get the Base URL (not according to the manual)

--- Backend/Services/URIService.cs
+++ Backend/Services/URIService.cs
@@ -1,5 +1,9 @@
 ﻿using MentorApp.Filter;
+using MentorApp.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +11,14 @@ using System.Threading.Tasks;
 
 namespace MentorApp.Services
 {
-    public class UriService : IUriService
+    public class URIService : IUriService
     {
         private readonly string _baseUri;
-        public UriService(string baseUri)
+        public URIService(string baseUri)
         {
             _baseUri = baseUri;
         }
+
         public Uri GetPageUri(PaginationFilter filter, string route)
         {
             var _enpointUri = new Uri(string.Concat(_baseUri, route));
@@ -21,5 +26,21 @@ namespace MentorApp.Services
             modifiedUri = QueryHelpers.AddQueryString(modifiedUri, "pageSize", filter.PageSize.ToString());
             return new Uri(modifiedUri);
         }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //?
+            services.AddDbContext<s17874Context>(opt => opt.UseSqlServer("Data Source=db-mssql;Initial Catalog=s17874;Persist Security Info=True;User ID=inzs17874;Password=admin"));
+            services.AddHttpContextAccessor();
+            services.AddSingleton<IUriService>(o =>
+            {
+                var accessor = o.GetRequiredService<IHttpContextAccessor>();
+                var request = accessor.HttpContext.Request;
+                var uri = string.Concat(request.Scheme, "://", request.Host.ToUriComponent());
+                return new URIService(uri);
+            });
+            services.AddControllers();
+        }
+
     }
 }
