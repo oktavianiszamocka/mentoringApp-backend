a32bc626e87803341c646849164970db046c253e
Tami <tami.kyanh@gmail.com>
2021-01-05 12:41:07 +0100

    Adding a GetProjectsById method

--- Backend/Repository/IProjectsListRepository.cs
+++ Backend/Repository/IProjectsListRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MentorApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace MentorApp.Repository
 {
     interface IProjectsListRepository
     {
+        Task<List<Project>> GetProjectsById(int idUser);
     }
 }
