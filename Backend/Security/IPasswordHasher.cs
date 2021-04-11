bbe3a0743cf907b120f5fa442f8ee78dd7dcd7ad
Tami <tami.kyanh@gmail.com>
2021-04-11 15:28:31 +0200

    Creating the security folder with the hashing classes

---
+++ Backend/Security/IPasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorApp.Security
+{
+    interface IPasswordHasher
+    {
+        string Hash(string password);
+
+        //tuples feature
+        (bool Verified, bool NeedsUpgrade) Check(string hash, string password);
+    }
+}
---
+++ Backend/Security/PasswordHasher.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace MentorApp.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private const int saltSize = 16; // 128 bit 
+        private const int keySize = 32; // 256 bit
+
+        public PasswordHasher(IOptions<HashingOptions> options)
+        {
+            Options = options.Value;
+        }
+
+        public string Hash(string password)
+        {
+            using (var algorithm = new Rfc2898DeriveBytes(
+              password,
+              saltSize,
+              Options.Iterations,
+              HashAlgorithmName.SHA256))
+            {
+                var key = Convert.ToBase64String(algorithm.GetBytes(keySize));
+                var salt = Convert.ToBase64String(algorithm.Salt);
+
+                return $"{Options.Iterations}.{salt}.{key}";
+            }
+        }
+
+        public (bool Verified, bool NeedsUpgrade) Check(string hash, string password)
+        {
+            var parts = hash.Split('.', 3);
+
+            if (parts.Length != 3)
+            {
+                throw new FormatException("Unexpected hash format. " +
+                  "Should be formatted as `{iterations}.{salt}.{hash}`");
+            }
+
+            var iterations = Convert.ToInt32(parts[0]);
+            var salt = Convert.FromBase64String(parts[1]);
+            var key = Convert.FromBase64String(parts[2]);
+
+            var needsUpgrade = iterations != Options.Iterations;
+
+            using (var algorithm = new Rfc2898DeriveBytes(
+              password,
+              salt,
+              iterations,
+              HashAlgorithmName.SHA256))
+            {
+                var keyToCheck = algorithm.GetBytes(keySize);
+
+                var verified = keyToCheck.SequenceEqual(key);
+
+                return (verified, needsUpgrade);
+            }
+        }
+
+        private HashingOptions Options { get; }
+    }
+}
