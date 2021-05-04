481b1299e87252ef9cc4ecebb206768cae6efeef
Oktaviani Puspita <44846145+oktavianiszamocka@users.noreply.github.com>
2021-05-04 23:49:04 +0700

    task endpoints

---
+++ Backend/Repository/ITaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MentorApp.DTOs.Responses;
+using MentorApp.Models;
+
+
+namespace MentorApp.Repository
+{
+    public interface ITaskRepository
+    {
+        Task<List<Models.Task>> GetTasksByProject(int idProject);
+
+        Task<Models.Task> GetTaskById(int idTask);
+
+    }
+}
