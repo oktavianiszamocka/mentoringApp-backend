0800d2b1cf7d61b1dddcff83cd41914e1b1501c8
Oktaviani Puspita <44846145+oktavianiszamocka@users.noreply.github.com>
2021-09-14 18:36:04 +0200

    change the dto

--- Backend/DTOs/Responses/ProjectMemberDTO.cs
+++ Backend/DTOs/Responses/ProjectMemberDTO.cs
@@ -5,11 +5,8 @@
         public int IdUser { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
-        public string Email{ get; set; }
         public string Avatar { get; set; }
-        public int  IdProjectMember { get; set; }
         public string ProjectRole { get; set; }
-        public int Role { get; set; }
         public string Major { get; set; }
         public int? Semester { get; set; }
 
